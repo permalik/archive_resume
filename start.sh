9b686dceca1309c58987eb0f0479fbfba2a5a8e3
permalik <permalik@protonmail.com>
2024-11-02 07:37:49 +0000

    feat: update deploy script

--- start.sh
+++ start.sh
@@ -4,5 +4,8 @@ echo "archive_resume: removing build..."
 sudo rm -rf /var/www/tymalik-swe.com/html/*
 sudo rm -rf /var/www/tymalik-swe.com/html/.*
 
+echo "archive_resume: installing dependencies..."
+pnpm i
+
 echo "archive_resume: building application..."
 pnpm build
