68a969b2de5ed171f171e9da87b39c356a865a42
permalik <permalik@protonmail.com>
2024-11-06 21:15:07 -0600

    update

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ sudo rm -rf /var/www/tymalik-swe.com/html/*
 sudo rm -rf /var/www/tymalik-swe.com/html/.*
 
 echo "archive_resume: installing dependencies..."
-pnpm i
+pnpm i --force
 
 echo "archive_resume: building application..."
 pnpm build
