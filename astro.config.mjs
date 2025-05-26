93b4e2e9b3ed31a1d41850bf5050cb7e06404f09
permalik <permalik@protonmail.com>
2025-05-25 20:21:12 -0500

    feat: configure netlify deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-    outDir: '/var/www/tymalik-swe.com/html/'
+  outDir: '/var/www/tymalik-swe.com/html/',
+  output: "server",
+  adapter: netlify()
 });
